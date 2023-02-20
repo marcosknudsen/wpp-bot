1cff1bda4ce8398d89618f67c493ffeaed7650c4
Marcos Knudsen <marcosknu14@gmail.com>
2023-02-20 02:36:06 -0300

    case instead of if else

--- index.js
+++ index.js
@@ -31,45 +31,78 @@ client.on("ready", async () => {
 client.on("message", async (msg) => {
   if (msg.body[0] == "!") {
     let [command, arg] = getCommands(msg.body);
-    if (command == "streams") {
-      client.sendMessage(msg.from, await getLiveStreams(streamers));
-    } else if (command == "isOn" && admins.includes(msg.from)) {
-      let light = parseInt(arg);
-      if (!isNaN(light))
+    switch (command) {
+      case "streams":
+        client.sendMessage(msg.from, await getLiveStreams(streamers));
+        break;
+      case "isOn":
+        if (admins.includes(msg.from)) {
+          let light = parseInt(arg);
+          if (!isNaN(light))
+            client.sendMessage(
+              msg.from,
+              (await isOn(light)) ? "Encendidas" : "Apagadas"
+            );
+        }
+        break;
+      case "turnOn":
+        if (admins.includes(msg.from)) {
+          let light = parseInt(arg);
+          if (!isNaN(light)) turnOn(light);
+        }
+        break;
+      case "turnOff":
+        if (admins.includes(msg.from)) {
+          let light = parseInt(arg);
+          if (!isNaN(light)) turnOff(light);
+        }
+        break;
+      case "aldosivi":
+        client.sendMessage(msg.from, await getMatches(22));
+        break;
+      case "matches":
+        let team = parseInt(arg);
+        if (isNaN(team))
+          client.sendMessage(
+            msg.from,
+            "Error: Compruebe haber insertado el team id correctamente (!matches {id})"
+          );
+        else client.sendMessage(msg.from, await getMatches(team));
+        break;
+      case "login":
+        if (arg == process.env.PASSWORD && !admins.includes(msg.from)) {
+          client.sendMessage(msg.from, "Sucessfully loged");
+          admins.push(msg.from);
+          await storage.set("admins", admins);
+        }
+        break;
+      case "image":
+        const mMedia = await wpp.MessageMedia.fromUrl(await getImage(arg));
+        client.sendMessage(msg.from, mMedia);
+        break;
+      case "help":
         client.sendMessage(
           msg.from,
-          (await isOn(light)) ? "Encendidas" : "Apagadas"
+          "🤖 Comandos:\n-!streams\n-!aldosivi\n-!matches {team id} (ex:!matches 5)\n-!image {description}(ex:!image messi)\n-!login password"
         );
-    } else if (command == "turnOn" && admins.includes(msg.from)) {
-      let light = parseInt(arg);
-      if (!isNaN(light)) turnOn(light);
-    } else if (command == "turnOff" && admins.includes(msg.from)) {
-      let light = parseInt(arg);
-      if (!isNaN(light)) turnOff(light);
-    } else if (command == "aldosivi") {
-      client.sendMessage(msg.from, await getMatches(22));
-    } else if (command == "matches") {
-      let team = parseInt(arg);
-      if (isNaN(team))
-        client.sendMessage(
-          msg.from,
-          "Error: Compruebe haber insertado el team id correctamente (!matches {id})"
-        );
-      else client.sendMessage(msg.from, await getMatches(team));
-    } else if (command == "login") {
-      if (arg == process.env.PASSWORD && !admins.includes(msg.from)) {
-        client.sendMessage(msg.from, "Sucessfully loged");
-        admins.push(msg.from);
-        await storage.set("admins", admins);
-      }
-    } else if (command == "image") {
-      const mMedia = await wpp.MessageMedia.fromUrl(await getImage(arg));
-      client.sendMessage(msg.from, mMedia);
-    } else if (command == "help") {
-      client.sendMessage(
-        msg.from,
-        "🤖 Comandos:\n-!streams\n-!aldosivi\n-!matches {team id} (ex:!matches 5)\n-!image {description}(ex:!image messi)\n-!login password"
-      );
+        break;
+      case "addStreamer":
+        if (
+          !streamers.map((x) => x.toUpperCase()).includes(arg.toUpperCase())
+        ) {
+          streamers.push(arg);
+          storage.set("streamers", streamers);
+        }
+        break;
+      case "removeStreamer":
+        var index = streamers
+          .map((x) => x.toUpperCase())
+          .indexOf(arg.toUpperCase());
+        if (index !== -1) {
+          streamers.splice(index, 1);
+        }
+        storage.set("streamers", streamers);
+        break;
     }
   }
 });
